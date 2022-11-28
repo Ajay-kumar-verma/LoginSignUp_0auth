95627f33150c3cd399d4b770bf89fd34c516c479
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-11-29 02:25:25 +0530

    Googleloginlogout

--- signinsignout/src/App.js
+++ signinsignout/src/App.js
@@ -1,9 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useEffect} from 'react';
+import { GoogleLogin ,GoogleLogout} from 'react-google-login'; 
+import { gapi } from 'gapi-script';
+import { useGoogleOneTapLogin } from 'react-google-one-tap-login';
 function App() {
+const  clientId="614668011518-d4g0cr825vs5ugm0iqgo5ieovqhgiisr.apps.googleusercontent.com";
+    
+useEffect(()=>{
+  gapi.load('client:auth2',()=>{
+    gapi.auth2.init({clientId})
+  })
+
+ 
+
+},[])
+
+
+  const responseGoogle = (response) => {
+    console.log({response});
+  }
+
+  // This code is onetap login 
+  useGoogleOneTapLogin({
+    onError: error => console.log(error),
+    onSuccess: response => console.log(response),
+    googleAccountConfigs: {
+      client_id:clientId 
+    }
+  })
+
   return (
-    <>Hello world</>
+    <>
+    <GoogleLogin
+    clientId={clientId}
+    render={renderProps => (
+      <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+    )}
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
+    
+    
+    <GoogleLogout
+    clientId={clientId}
+     buttonText="LOGOUT OUT"
+     onLogoutSuccess={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+    
+    />
+    
+    </>
   );
 }
 
